644061188b12140b6e126a828879e313944e6ccb
gunnvrs <gunn2545@gmail.com>
2022-12-22 18:41:14 +0700

    first commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,24 +4,40 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  return false if arr.empty? && n.zero?
+  arr.combination(2).any? {|a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  hello(name)
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if /[aeiouAEIOU*]/.match(s[0])
+    return false
+  elsif /0-9*/.match(s[0])
+    return false
+  elsif /[^a-zA-Z0-9.*]/.match(s)
+    return false
+  elsif /^$/.match(s)
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
